54ac547dff4aaa04572b226547b78f477c9e49f0
Lime_Ivy <raimu10311221@yahoo.co.jp>
2025-10-02 15:50:21 +0900

    ロゴの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex items-center justify-center h-screen bg-gray-800">
+      <div className="w-1/2 flex items-center justify-center mx-auto">
+        <Image src="/wordsence.png" alt="logo" width={700} height={700} className="object-cover" />
+      </div>
+      <div className="flex items-center justify-center w-1/2">
+        <Button>Click me</Button>
+      </div>
     </div>
   );
 }
