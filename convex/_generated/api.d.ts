e3ec26586d6f30de0828a0b065ead90eefefaf62
LimeIvy <raimu10311221@yahoo.co.jp>
2025-10-18 19:18:21 +0900

    convexとclerkを接続

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,12 +8,13 @@
  * @module
  */
 
+import type * as card from "../card.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as card from "../card.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -26,11 +27,15 @@ import type * as card from "../card.js";
 declare const fullApi: ApiFromModules<{
   card: typeof card;
 }>;
+declare const fullApiWithMounts: typeof fullApi;
+
 export declare const api: FilterApi<
-  typeof fullApi,
+  typeof fullApiWithMounts,
   FunctionReference<any, "public">
 >;
 export declare const internal: FilterApi<
-  typeof fullApi,
+  typeof fullApiWithMounts,
   FunctionReference<any, "internal">
 >;
+
+export declare const components: {};
