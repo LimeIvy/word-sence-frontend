ac4b9570d3b8738dfa0cef272e9149afd4187fbd
LimeIvy <raimu10311221@yahoo.co.jp>
2025-10-21 01:05:51 +0900

    userテーブルにclerkのidでのindexを追加

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -9,6 +9,7 @@
  */
 
 import type * as card from "../card.js";
+import type * as user from "../user.js";
 
 import type {
   ApiFromModules,
@@ -26,6 +27,7 @@ import type {
  */
 declare const fullApi: ApiFromModules<{
   card: typeof card;
+  user: typeof user;
 }>;
 declare const fullApiWithMounts: typeof fullApi;
 
